107e7d820bce61d22d3ec4fc6cde466ed7d9973a
Nrittik Sarmah <nrittik43@gmail.com>
2021-12-28 20:30:23 +0530

    toggle changed and utilities created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utilities/formatMessage');
 
 const app = express();
 const server = http.createServer(app);
@@ -14,21 +15,21 @@ io.on('connection', socket=> {
     console.log("New Connection made");
 
     // message to single client/ client making the connection
-    socket.emit('message', 'welcome to Firefirst');
+    socket.emit('message', formatMessage('Fire Bot', 'Welcome to Firefirst'));
 
     // broadcast a user connection to everyone except the client making the connection
-    socket.broadcast.emit('message', 'Someone hoped into the server');
+    socket.broadcast.emit('message', formatMessage('Fire Bot', 'Someone hoped into the server'));
 
     //message to everyone
     // io.emit('message', 'Someone entered the chat room');
     socket.on('disconnect', ()=>{
-        io.emit('message', 'someone left the chat room');
+        io.emit('message', formatMessage('Fire Bot', 'Someone left  the server'));
     });
 
     // catching message in the server
     socket.on('chatMsg', message=>{
         // console.log(message);
-        io.emit('message', message);
+        io.emit('message',formatMessage('User', message));
     })
 })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
-const menuToggler = document.querySelector('.menu-toggle');
+const menuToggler = document.querySelector('.menu-toggle-down');
 const chatSidebar = document.querySelector('.chat-sidebar');
 const socket = io();
+
+const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true} );
+console.log(username, room);
 
 // catching message on the frontend
 socket.on('message', message =>{
@@ -18,8 +21,8 @@ menuToggler.addEventListener('click', ()=> {
     chatSidebar.classList.toggle('chat-sidebar');
     chatSidebar.classList.toggle('offcanvas');
 
-    menuToggler.classList.toggle('fa-chevron-circle-down');
-    menuToggler.classList.toggle('fa-chevron-up')
+    menuToggler.classList.toggle('menu-toggle-down');
+    menuToggler.classList.toggle('menu-toggle-up')
 })
 
 // auto adding and removing of offcanvas sidebar based on window size
@@ -29,8 +32,8 @@ window.addEventListener('resize', ()=> {
         if(!chatSidebar.classList.contains('chat-sidebar')){
             chatSidebar.classList.toggle('chat-sidebar');
         }
-        menuToggler.classList.add('fa-chevron-circle-down');
-        menuToggler.classList.remove('fa-chevron-up')
+        menuToggler.classList.add('menu-toggle-down');
+        menuToggler.classList.remove('menu-toggle-up')
     }
 })
 
@@ -48,9 +51,9 @@ function displayMessage(message) {
     const div = document.createElement('div');
     div.classList.add('message');
     div.innerHTML = `
-    <p class="meta">Brad <span>9:12pm</span></p>
+    <p class="meta">${message.username} <span>${message.time}</span></p>
     <p class="text">
-        ${message}
+        ${message.message}
     </p>`;
     document.querySelector('.chat-messages').appendChild(div);
     chatForm.reset();
